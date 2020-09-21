15f72cd732f5dde383ea0640df09618aeef3a322
Aidar-K <ak.aidar.kasymbekov@gmail.com>
2020-09-21 23:14:03 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Boss boss = new Boss();
+        boss.setBossDamge(50);
+        boss.setBossHealth(100);
+        boss.setBossDefence("Magic");
+
+        System.out.println("Характеристики босса:" + " " + boss.getBossDamge() + " " +
+                boss.getBossHealth() + " " + boss.getBossDefence());
+    }
+}
